8cd04e30f9d881a94d09cd1940b4311c06f7f0fb
Andreja Trajkovic <andreja.trajkovic@crowdbotics.com>
2023-08-10 16:43:18 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "my-artics-42490",
-  url: "https://my-artics-42490.botics.co",
-  api: "https://my-artics-42490.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "my-artics-42490",
+  "url": "https://my-artics-42490.botics.co",
+  "api": "https://my-artics-42490.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "ScreenAI10"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
